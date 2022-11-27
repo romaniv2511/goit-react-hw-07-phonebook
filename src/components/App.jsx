8900a744e6698fe89f834660fa3544302e8fa9c8
romaniv2511 <romaniv2511@gmail.com>
2022-11-27 16:21:37 +0200

    add form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import AddContactForm from './Form/Form';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <AddContactForm />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,92 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import styled from 'styled-components';
+
+const FormStyled = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding: 16px 12px;
+  border: 2px solid black;
+  border-radius: 2px;
+  margin-bottom: 16px;
+  max-width: 400px;
+`;
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  height: 92px;
+`;
+const Input = styled(Field)`
+  border: 1px solid black;
+  border-radius: 4px;
+
+  font-size: 16px;
+  padding: 4px 8px;
+`;
+const Error = styled(ErrorMessage)`
+  font-size: 12px;
+  color: red;
+`;
+const Btn = styled.button`
+  padding: 8px 12px;
+  border: none;
+  border-radius: 4px;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 1.2;
+  background-color: lightGrey;
+  cursor: pointer;
+  &:hover,
+  &:focus {
+    background-color: darkBlue;
+    color: white;
+  }
+`;
+
+const schema = Yup.object().shape({
+  name: Yup.string()
+    .matches(
+      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      'Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore'
+    )
+    .required(),
+  number: Yup.string()
+    .matches(
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    )
+    .required(),
+});
+
+const AddContactForm = () => {
+  const handleSubmit = (values, { resetForm }) => {
+    console.log(values);
+
+    resetForm();
+  };
+  return (
+    <Formik
+      initialValues={{ name: '', number: '' }}
+      onSubmit={handleSubmit}
+      validationSchema={schema}
+    >
+      <FormStyled>
+        <Label htmlFor="name">
+          Name
+          <Input id="name" name="name" />
+          <Error component="span" name="name" />
+        </Label>
+        <Label htmlFor="number">
+          Number
+          <Input id="number" name="number" type="tel" />
+          <Error component="span" name="number" />
+        </Label>
+        <Btn type="submit">Add contact</Btn>
+      </FormStyled>
+    </Formik>
+  );
+};
+
+export default AddContactForm;
